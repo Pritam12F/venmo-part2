0aee52428e9aa4b75984a70673a685ec047c1158
PritamDas <pritam.das.santu@gmail.com>
2024-05-18 16:09:41 +0530

    added proper server action for p2p

--- apps/user-app/app/lib/actions.ts
+++ apps/user-app/app/lib/actions.ts
@@ -3,7 +3,6 @@
 import prisma from "@repo/db/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth";
-import { z } from "zod";
 
 export async function onRampTransaction(amount: number, provider: string) {
   const session = await getServerSession(authOptions);
@@ -38,27 +37,69 @@ export async function onRampTransaction(amount: number, provider: string) {
   }
 }
 
-export async function p2phandler(amount, timestamp, fromUserId, toUserId) {
-  const p2pSchema = z.object({
-    amount: z.number(),
-    timestamp: z.number(),
-    fromUserId: z.number(),
-    toUserId: z.number(),
-  });
+export async function p2phandler(to: string, amount: number) {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
 
-  const { success } = p2pSchema.safeParse({
-    amount,
-    timestamp,
-    fromUserId,
-    toUserId,
-  });
-
-  if (!success) {
+  if (!userId) {
     return {
-      message: "Invalid inputs",
+      message: "Not authorized",
     };
   }
 
-  const session = await getServerSession(authOptions);
-  const userId = session?.user?.id;
+  const toUser = await prisma.user.findFirst({
+    where: {
+      number: to,
+    },
+  });
+
+  if (!toUser) {
+    return {
+      message: "User doesn't exist",
+    };
+  }
+
+  try {
+    await prisma.$transaction([
+      prisma.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(userId)} FOR UPDATE`,
+      prisma.balance.update({
+        where: {
+          userId,
+        },
+        data: {
+          amount: {
+            decrement: amount,
+          },
+        },
+      }),
+
+      prisma.balance.update({
+        where: {
+          userId: Number(to),
+        },
+        data: {
+          amount: {
+            increment: amount,
+          },
+        },
+      }),
+
+      prisma.p2pTransfer.create({
+        data: {
+          amount,
+          timestamp: new Date(),
+          fromUserId: userId,
+          toUserId: Number(to),
+        },
+      }),
+    ]);
+
+    return {
+      message: "Transaction was successful",
+    };
+  } catch (err) {
+    return {
+      message: "Some error occured while making the transaction",
+    };
+  }
 }

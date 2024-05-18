2355ac63c6eef2efca64ea4fd3afaee8c6b13549
PritamDas <pritam.das.santu@gmail.com>
2024-05-18 12:51:14 +0530

    added p2p table in prisma schema

--- apps/user-app/app/lib/actions.ts
+++ apps/user-app/app/lib/actions.ts
@@ -3,6 +3,7 @@
 import prisma from "@repo/db/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth";
+import { z } from "zod";
 
 export async function onRampTransaction(amount: number, provider: string) {
   const session = await getServerSession(authOptions);
@@ -35,4 +36,29 @@ export async function onRampTransaction(amount: number, provider: string) {
       message: "Some error occured",
     };
   }
+}
+
+export async function p2phandler(amount, timestamp, fromUserId, toUserId) {
+  const p2pSchema = z.object({
+    amount: z.number(),
+    timestamp: z.number(),
+    fromUserId: z.number(),
+    toUserId: z.number(),
+  });
+
+  const { success } = p2pSchema.safeParse({
+    amount,
+    timestamp,
+    fromUserId,
+    toUserId,
+  });
+
+  if (!success) {
+    return {
+      message: "Invalid inputs",
+    };
+  }
+
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
 }
